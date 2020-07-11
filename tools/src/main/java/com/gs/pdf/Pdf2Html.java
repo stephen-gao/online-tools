2711c9a80dda4e0dd9eb805a9b9164c5a3f20d82
gaosheng <gaosheng@253.com>
2020-07-11 11:37:45 +0800

    some

---
+++ tools/src/main/java/com/gs/pdf/Pdf2Html.java
@@ -0,0 +1,12 @@
+package com.gs.pdf;
+
+/**
+ * ...
+ *
+ * @author GaoSheng
+ * @version 1.0
+ * @blame GaoSheng
+ * @since 2020/07/11 09:41
+ **/
+public class Pdf2Html {
+}
---
+++ tools/src/main/java/com/gs/word/Word2Txt.java
@@ -0,0 +1,12 @@
+package com.gs.word;
+
+/**
+ * ...
+ *
+ * @author GaoSheng
+ * @version 1.0
+ * @blame GaoSheng
+ * @since 2020/07/11 11:19
+ **/
+public class Word2Txt {
+}
